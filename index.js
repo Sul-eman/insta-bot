a811a125312e7724e74d70242b2204e334e16017
Sul-eman <msulemanhere@gmail.com>
2021-02-19 09:18:39 -0600

    connected to brainshop's api

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const Insta = require("@androz2091/insta.js");
+const fetch = require("node-fetch");
 const dotenv = require("dotenv");
 
 dotenv.config();
@@ -13,10 +14,21 @@ client.on("messageCreate", (message) => {
 	if (message.author.id === client.user.id) return;
 
 	message.markSeen();
+	fetch(
+		`http://api.brainshop.ai/get?bid=${process.env.BID}&key=${process.env.BOT_KEY}&uid=${client.user.id}&msg=${message.content}`
+	)
+		.then((res) => res.json())
+		.then((json) => {
+			if (!json.cnt) {
+				return;
+			}
+			return message.reply(json.cnt);
+		})
+		.catch((err) => {});
 
-	if (message.content === "!ping") {
-		message.reply("!pong");
-	}
+	// if (message.content === "!ping") {
+	// 	message.reply("!pong");
+	// }
 });
 
-client.login("simpy4serotonin", process.env.PASSWORD);
+client.login(process.env.INSTA_USER, process.env.PASSWORD);
